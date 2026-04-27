52f4018207aa8cd5c723f58f617f73a9f38736aa
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-27 11:36:31 +0700

    pembuatan class pelajar private dan public.cpp

---
+++ pembuatan class pelajar private dan public.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class pelajar; // Forward declaration
+
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x); // Friend function
+};
