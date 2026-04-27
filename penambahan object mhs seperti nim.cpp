1a8dd01044dafb85643a3e0a1829c7f45d1c5e4f
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-27 11:46:59 +0700

    penambahan object mhs seperti nim.cpp

--- penambahan object mhs seperti nim.cpp
+++ penambahan object mhs seperti nim.cpp
@@ -12,3 +12,8 @@ public:
 int main() {
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); // Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    refMhs.showNim();
+}
